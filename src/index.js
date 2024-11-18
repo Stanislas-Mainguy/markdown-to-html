74ca067619c34c813cccb6697af5c97d2ff89c84
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-11-18 17:11:56 +0100

    fix(index): update import from main.scss to main.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'normalize.css';
-import './styles/main.scss';
+import './styles/main.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
