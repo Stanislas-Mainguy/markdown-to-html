f21b177ea9d6f54fe6245a0db8b24e40b597a25e
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-11-18 17:11:45 +0100

    feat(App): add imports and code for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SideMenu from './components/organisms/sideMenu/SideMenu';
+import HeaderInfo from './components/organisms/headerInfo/HeaderInfo';
+import MarkdownEditor from './components/molecules/markdownEditor/MarkdownEditor';
+import HtmlPreview from './components/molecules/htmlPreview/HtmlPreview';
+import './styles/main.css';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Markdown to HTML</h1>
-    </div>
-  );
+    const [markdown, setMarkdown] = useState('');
+
+    return (
+        <div className="app-container">
+            <HeaderInfo />
+            <SideMenu />
+            <div className="content">
+                <h1>Markdown to HTML Converter</h1>
+                <MarkdownEditor value={markdown} onChange={setMarkdown} />
+                <HtmlPreview markdown={markdown} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
+
+
